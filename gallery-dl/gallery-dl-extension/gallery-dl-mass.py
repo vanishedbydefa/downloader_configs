08f164efc916a876f9d2a06a567d4002f3187d5c
vanishedbydefa <128320031+vanishedbydefa@users.noreply.github.com>
2024-02-11 14:27:30 +0100

    Update gallery-dl-mass.py

--- gallery-dl/gallery-dl-extension/gallery-dl-mass.py
+++ gallery-dl/gallery-dl-extension/gallery-dl-mass.py
@@ -1,10 +1,12 @@
 import subprocess
+import os
 
 #virtual env activation command
-virtualenv_activation_command = 'call .venv\\Scripts\\activate.bat'
+#virtualenv_activation_command = 'call .venv\\Scripts\\activate.bat'
+virtualenv_activation_command = '.venv\\Scripts\\activate'
 
 profiles = []
-with open('profiles.txt', "r") as f:
+with open('twitter_profiles.txt', "r") as f:
     profiles = [line.rstrip('\n') for line in f.readlines() if line.strip()]
     f.close()
 
@@ -22,31 +24,19 @@ while True:
     
     # Run activation and download commands in a single subprocess
     command = f'{virtualenv_activation_command} && {download_command}'
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
-    while True:
-        output = process.stdout.readline()
-        if output == '' and process.poll() is not None:
-            break
-        if output:
-            print(output.strip())
-
-    # Wait for the subprocess to finish
-    process.wait()
-
+    os.system(command)
+      
     
-    if process.returncode == 0:
-        print(f"Downloading {profilename} executed successfully")
-        #write profile to downloaded profiles
-        with open("profiles_done.txt", 'a+') as f:
-            f.write(profile + '\n')
-        
-        #remove profile from to download profiles
-        profiles = [line for line in profiles if line.strip() != profile.strip()]
+    print(f"Downloading {profilename} executed successfully")
+    #write profile to downloaded profiles
+    with open("twitter_profiles_done.txt", 'a+') as f:
+        f.write(profile + '\n')
+    
+    #remove profile from to download profiles
+    profiles = [line for line in profiles if line.strip() != profile.strip()]
 
-        # Write the updated contents back to the file
-        with open("profiles.txt", 'w') as f:
-            for profile in profiles:
-                f.write(profile)
-    else:
-        print(f"Downloading {profilename} failed with return code {process.returncode}")
+    # Write the updated contents back to the file
+    with open("twitter_profiles.txt", 'w') as f:
+        for profile in profiles:
+            f.write(profile)
